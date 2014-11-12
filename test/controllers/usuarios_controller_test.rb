a015e29eb3c544448d5af658c9b69bf402b91e63
Gerardo Estrada <gerardo14@gerardo-RV410.(none)>
2014-11-12 11:57:57 -0300

    fix names controller

--- test/controllers/usuarios_controller_test.rb
+++ test/controllers/usuarios_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class PostControllerTest < ActionController::TestCase
+class UsuariosControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
