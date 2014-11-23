f1083be8fe40bee9034a7f67e102f5f0b574bf99
gustavo000 <fenriz0006@gmail.com>
2014-11-22 21:43:27 -0300

    fix

--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 20141116031924) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "username"
+    t.string   "nombre"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+  Usuario.create([{ nombre: 'Gustavo', email: 'email1@email.com' , password: 'password' })
